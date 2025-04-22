dd5ade9370f181ff6acde851bcb931c78b983ec7
Callum Seagram Airlie <143629409+seagram@users.noreply.github.com>
2025-04-22 11:59:09 -0400

    Added native virtual text

--- nvim/lsp/luals.lua
+++ nvim/lsp/luals.lua
@@ -3,4 +3,15 @@ return {
 	cmd = { "lua-language-server" },
 	filetypes = { "lua" },
 	root_markers = { ".luarc.json", ".luarc.jsonc" },
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 }
